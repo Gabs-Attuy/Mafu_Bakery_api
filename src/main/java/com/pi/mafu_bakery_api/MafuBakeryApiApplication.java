55a8bc942536f83922f5fe019fbc58653937561f
Gabs-Attuy <gabrielattuy1010@gmail.com>
2024-08-23 16:00:15 -0300

    Inicializando projeto

---
+++ src/main/java/com/pi/mafu_bakery_api/MafuBakeryApiApplication.java
@@ -0,0 +1,13 @@
+package com.pi.mafu_bakery_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MafuBakeryApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MafuBakeryApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pi/mafu_bakery_api/MafuBakeryApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pi.mafu_bakery_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MafuBakeryApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
