e536d709b83f78dd7ea12d6d814d9c0f42ac2b87
jcavalcantee <jcavalcantessilva@gmail.com>
2024-09-01 15:17:55 -0300

    refactor: metodo de cadastro de usuario

--- src/main/java/com/pi/mafu_bakery_api/repository/PermissaoRepository.java
+++ src/main/java/com/pi/mafu_bakery_api/repository/PermissaoRepository.java
@@ -1,12 +1,15 @@
 package com.pi.mafu_bakery_api.repository;
 
+import com.pi.mafu_bakery_api.enums.RoleEnum;
 import com.pi.mafu_bakery_api.model.Permissao;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 public interface PermissaoRepository extends JpaRepository<Permissao, Long> {
-
     @Query("SELECT p FROM Permissao p WHERE p.id = :id")
     Permissao findPermissaoById(@Param("id") Long id);
+
+    @Query("SELECT p FROM Permissao p WHERE p.permissao = :permissao")
+    Permissao findPermissaoByNome(@Param("permissao") String permissao);
 }
--- src/main/java/com/pi/mafu_bakery_api/service/UsuarioService.java
+++ src/main/java/com/pi/mafu_bakery_api/service/UsuarioService.java
@@ -6,6 +6,7 @@ import com.pi.mafu_bakery_api.dto.ListaUsuariosDTO;
 import com.pi.mafu_bakery_api.enums.RoleEnum;
 import com.pi.mafu_bakery_api.model.*;
 import com.pi.mafu_bakery_api.repository.*;
+import jakarta.transaction.Transactional;
 import org.apache.coyote.Response;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -32,6 +33,35 @@ public class UsuarioService {
     @Autowired
     private PermissaoRepository permissaoRepository;
 
+    @Transactional
+    public ResponseEntity<Usuario> cadastrarUsuario(CadastroUsuarioDTO dto) throws Exception {
+
+        if(checaSeOsParametrosDeEntradaNaoSaoNulos(dto))
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+
+        Usuario usuario = new Usuario();
+        usuario.setNome(dto.getNome());
+        usuario.setCpf(dto.getCpf());
+        usuarioRepository.save(usuario);
+
+        Credencial credencial = new Credencial();
+        credencial.setUsuario(usuario);
+        credencial.setEmail(dto.getEmail());
+        credencial.setSenha(encryptPassword(dto.getSenha()));
+
+        RoleEnum roleEnum = RoleEnum.valueOf(String.valueOf(dto.getPermissao()));
+        Permissao permissao = permissaoRepository.findPermissaoByNome(String.valueOf(roleEnum));
+
+        credencial.setPermissao(permissao);
+        credencialRepository.save(credencial);
+        return new ResponseEntity<>(HttpStatus.CREATED);
+    }
+
+    private boolean checaSeOsParametrosDeEntradaNaoSaoNulos(CadastroUsuarioDTO dto) {
+        return dto == null || dto.getNome() == null || dto.getCpf() == null ||
+                dto.getEmail() == null || dto.getSenha() == null || dto.getPermissao() == null;
+    }
+
     public ResponseEntity<Usuario> cadastrarCliente(CadastroUsuarioDTO dto) throws Exception {
 
         Usuario usuario = new Usuario();
@@ -47,7 +77,7 @@ public class UsuarioService {
         credencial.setUsuario(id);
         credencial.setEmail(dto.getEmail());
         credencial.setSenha(encryptPassword(dto.getSenha()));
-        credencial.setIsEnabled(true);
+        credencial.setEnabled(true);
         Permissao permissao = new Permissao();
         permissao.setId(3L);
         permissao.setPermissao(RoleEnum.CLIENTE);
@@ -59,34 +89,6 @@ public class UsuarioService {
         carrinho.setQuantidadeItens(0);
         carrinhoRepository.save(carrinho);
 
-        return new ResponseEntity<>(HttpStatus.CREATED);
-    }
-
-    public ResponseEntity<Usuario> cadastrarUsuario(CadastroUsuarioDTO dto) throws Exception {
-        Usuario usuario = new Usuario();
-        Credencial credencial = new Credencial();
-
-        usuario.setNome(dto.getNome());
-        usuario.setCpf(dto.getCpf());
-        usuarioRepository.save(usuario);
-
-        Usuario id = usuarioRepository.findById(usuario.getId()).
-                orElseThrow(() -> new Exception("Usuário não encontrado"));
-
-        credencial.setUsuario(id);
-        credencial.setEmail(dto.getEmail());
-        credencial.setSenha(encryptPassword(dto.getSenha()));
-        credencial.setIsEnabled(true);
-        Permissao permissao = new Permissao();
-        RoleEnum roleEnum = RoleEnum.valueOf(String.valueOf(dto.getPermissao()));
-        permissao.setPermissao(roleEnum);
-
-        if (roleEnum == RoleEnum.ADMINISTRADOR)
-            permissao.setId(1L);
-        else
-            permissao.setId(2L);
-        credencial.setPermissao(permissao);
-        credencialRepository.save(credencial);
         return new ResponseEntity<>(HttpStatus.CREATED);
     }
 
