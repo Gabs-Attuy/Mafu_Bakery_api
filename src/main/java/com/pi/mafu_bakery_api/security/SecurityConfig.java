ae5702b04ccd72f750c9124158f2f5a93bc6de9f
jcavalcantee <jcavalcantessilva@gmail.com>
2024-09-28 17:37:13 -0300

    criado listas para controle de acesso aos endpoints

--- src/main/java/com/pi/mafu_bakery_api/security/SecurityConfig.java
+++ src/main/java/com/pi/mafu_bakery_api/security/SecurityConfig.java
@@ -24,37 +24,81 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain securityFilterChain (HttpSecurity http) throws Exception {
-        http
+
+        final String [] ENDPOINTS_LIBERADOS = {
+                "/api/auth/signin",
+                "/api/cliente",
+                "/api/usuarioLogado",
+                "/api/produtos",
+                "/api/produtos/exibirTodos"
+        };
+
+        final String [] ENDPOINTS_ADMINISTRADOR = {
+                "/api/usuario",
+                "/api/mp",
+                "/api/ativaDesativaUsuario",
+                "/api/alterarUsuario",
+                "/api/mp",
+                "/api/mp/statusMp",
+                "/api/mp/statusProduto",
+                "/api",
+                "/api/produtos/alterar",
+                "/api/usuariosPorPesquisa",
+                "/api/buscaUsuario",
+                "/api/produtos/exibicao",
+                "/api/produtos/alterar"
+        };
+
+        final String [] ENDPOINTS_ESTOQUISTA = {
+                "/api/mp/aumentarMp",
+                "/api/mp/consumirMp",
+                "/api/produtos/confeccionaProdutos",
+                "/api/listarPedidos"
+        };
+
+        final String [] ENDPOINTS_ADMINISTRADOR_ESTOQUISTA = {
+                "/api/listarProdutos",
+                "/api/produtos/recuperaProduto",
+                "/api/produtos/listagem",
+                "/api/produtos/buscarNome",
+                "/api/mp/mpPorId"
+        };
+
+                http
                 .csrf(csfr -> csfr.disable())
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(authorize -> authorize
-                        .requestMatchers(HttpMethod.POST, "/api/auth/signin").permitAll()
-                        .requestMatchers(HttpMethod.POST, "/api/usuario").hasAuthority("ADMINISTRADOR")
-                        .requestMatchers(HttpMethod.POST, "/api/cliente").permitAll()
-                        .requestMatchers(HttpMethod.POST, "/api/mp").hasAuthority("ADMINISTRADOR")
-                        .requestMatchers(HttpMethod.PATCH, "/api/ativaDesativaUsuario").hasAuthority("ADMINISTRADOR")
-                        .requestMatchers(HttpMethod.PATCH, "/api/alterarUsuario").hasAuthority("ADMINISTRADOR")
-                        .requestMatchers(HttpMethod.PATCH, "/api/mp").hasAuthority("ADMINISTRADOR")
-                        .requestMatchers(HttpMethod.PATCH, "/api/mp/statusMp").hasAuthority("ADMINISTRADOR")
-                        .requestMatchers(HttpMethod.PATCH, "/api/mp/statusProduto").hasAuthority("ADMINISTRADOR")
-                        .requestMatchers(HttpMethod.PATCH, "/api/mp/aumentarMp").hasAuthority("ESTOQUISTA")
-                        .requestMatchers(HttpMethod.PATCH, "/api/mp/consumirMp").hasAuthority("ESTOQUISTA")
-                        .requestMatchers(HttpMethod.PATCH, "/api/produtos/confeccionaProdutos").hasAuthority("ESTOQUISTA")
-                        .requestMatchers(HttpMethod.PATCH, "/api/produtos/alterar").hasAuthority("ADMINISTRADOR")
-                        .requestMatchers(HttpMethod.GET, "/api/usuarioLogado").permitAll()
-                        .requestMatchers(HttpMethod.GET, "/api").hasAuthority("ADMINISTRADOR")
-                        .requestMatchers(HttpMethod.GET, "/api/listarPedidos").hasAuthority("ESTOQUISTA")
-                        .requestMatchers(HttpMethod.GET, "/api/listarProdutos").hasAnyAuthority("ADMINISTRADOR", "ESTOQUISTA")
-                        .requestMatchers(HttpMethod.GET, "/api/usuariosPorPesquisa").hasAuthority("ADMINISTRADOR")
-                        .requestMatchers(HttpMethod.GET, "/api/buscaUsuario").hasAuthority("ADMINISTRADOR")
-                        .requestMatchers(HttpMethod.GET, "/api/produtos/recuperaProduto").hasAnyAuthority("ADMINISTRADOR", "ESTOQUISTA")
-                        .requestMatchers(HttpMethod.GET, "/api/produtos/listagem").hasAnyAuthority("ADMINISTRADOR", "ESTOQUISTA")
-                        .requestMatchers(HttpMethod.GET, "/api/produtos/buscarNome").hasAnyAuthority("ADMINISTRADOR", "ESTOQUISTA")
-                        .requestMatchers(HttpMethod.GET, "/api/produtos/exibicao").hasAuthority("ADMINISTRADOR")
-                        .requestMatchers(HttpMethod.GET, "/api/mp/mpPorId").hasAnyAuthority("ADMINISTRADOR", "ESTOQUISTA")
-                        .requestMatchers(HttpMethod.GET, "/api/produtos/alterar").hasAuthority("ADMINISTRADOR")
-                        .requestMatchers(HttpMethod.GET, "/api/produtos").permitAll()
-                        .requestMatchers(HttpMethod.GET, "/api/produtos/exibirTodos").permitAll()
+                          .requestMatchers(ENDPOINTS_ESTOQUISTA).hasAuthority("ESTOQUISTA")
+                          .requestMatchers(ENDPOINTS_ADMINISTRADOR).hasAuthority("ADMINISTRADOR")
+                          .requestMatchers(ENDPOINTS_ADMINISTRADOR_ESTOQUISTA).hasAnyAuthority("ADMINISTRADOR", "ESTOQUISTA")
+                          .requestMatchers(ENDPOINTS_LIBERADOS).permitAll()
+//                        .requestMatchers(HttpMethod.POST, "/api/auth/signin").permitAll()
+//                        .requestMatchers(HttpMethod.POST, "/api/usuario").hasAuthority("ADMINISTRADOR")
+//                        .requestMatchers(HttpMethod.POST, "/api/cliente").permitAll()
+//                        .requestMatchers(HttpMethod.POST, "/api/mp").hasAuthority("ADMINISTRADOR")
+//                        .requestMatchers(HttpMethod.PATCH, "/api/ativaDesativaUsuario").hasAuthority("ADMINISTRADOR")
+//                        .requestMatchers(HttpMethod.PATCH, "/api/alterarUsuario").hasAuthority("ADMINISTRADOR")
+//                        .requestMatchers(HttpMethod.PATCH, "/api/mp").hasAuthority("ADMINISTRADOR")
+//                        .requestMatchers(HttpMethod.PATCH, "/api/mp/statusMp").hasAuthority("ADMINISTRADOR")
+//                        .requestMatchers(HttpMethod.PATCH, "/api/mp/statusProduto").hasAuthority("ADMINISTRADOR")
+//                        .requestMatchers(HttpMethod.PATCH, "/api/mp/aumentarMp").hasAuthority("ESTOQUISTA")
+//                        .requestMatchers(HttpMethod.PATCH, "/api/mp/consumirMp").hasAuthority("ESTOQUISTA")
+//                        .requestMatchers(HttpMethod.PATCH, "/api/produtos/confeccionaProdutos").hasAuthority("ESTOQUISTA")
+//                        .requestMatchers(HttpMethod.PATCH, "/api/produtos/alterar").hasAuthority("ADMINISTRADOR")
+//                        .requestMatchers(HttpMethod.GET, "/api/usuarioLogado").permitAll()
+//                        .requestMatchers(HttpMethod.GET, "/api").hasAuthority("ADMINISTRADOR")
+//                        .requestMatchers(HttpMethod.GET, "/api/listarPedidos").hasAuthority("ESTOQUISTA")
+//                        .requestMatchers(HttpMethod.GET, "/api/listarProdutos").hasAnyAuthority("ADMINISTRADOR", "ESTOQUISTA")
+//                        .requestMatchers(HttpMethod.GET, "/api/usuariosPorPesquisa").hasAuthority("ADMINISTRADOR")
+//                        .requestMatchers(HttpMethod.GET, "/api/buscaUsuario").hasAuthority("ADMINISTRADOR")
+//                        .requestMatchers(HttpMethod.GET, "/api/produtos/recuperaProduto").hasAnyAuthority("ADMINISTRADOR", "ESTOQUISTA")
+//                        .requestMatchers(HttpMethod.GET, "/api/produtos/listagem").hasAnyAuthority("ADMINISTRADOR", "ESTOQUISTA")
+//                        .requestMatchers(HttpMethod.GET, "/api/produtos/buscarNome").hasAnyAuthority("ADMINISTRADOR", "ESTOQUISTA")
+//                        .requestMatchers(HttpMethod.GET, "/api/produtos/exibicao").hasAuthority("ADMINISTRADOR")
+//                        .requestMatchers(HttpMethod.GET, "/api/mp/mpPorId").hasAnyAuthority("ADMINISTRADOR", "ESTOQUISTA")
+//                        .requestMatchers(HttpMethod.GET, "/api/produtos/alterar").hasAuthority("ADMINISTRADOR")
+//                        .requestMatchers(HttpMethod.GET, "/api/produtos").permitAll()
+//                        .requestMatchers(HttpMethod.GET, "/api/produtos/exibirTodos").permitAll()
                         .anyRequest().authenticated())
                 .addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class)
                 .cors(withDefaults()); // Adiciona suporte a CORS
